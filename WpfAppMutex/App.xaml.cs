13a005ba68029f38cfbccabd9f144afb6c477a9a
David Mordechai <david.mordechai79@gmail.com>
2024-01-16 17:55:41 +0200

    Add project files.

---
+++ WpfAppMutex/App.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace WpfAppMutex;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App
+{
+    private readonly ApplicationSingleInstanceGuard _applicationSingleInstanceGuard = new();
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        var appName = Assembly.GetExecutingAssembly().GetName().Name ?? "Moav.App";
+        _applicationSingleInstanceGuard.OnStartUp(appName);
+        base.OnStartup(e);
+    }
+
+    protected override void OnExit(ExitEventArgs e)
+    {
+        _applicationSingleInstanceGuard.OnExit();
+        base.OnExit(e);
+    }
+}
---
+++ WpfAppMutex/ApplicationSingleInstanceGuard.cs
@@ -0,0 +1,33 @@
+﻿using System.Security.AccessControl;
+using System.Security.Principal;
+using System.Windows;
+
+namespace WpfAppMutex;
+
+internal class ApplicationSingleInstanceGuard
+{
+    private Mutex? _mutex;
+
+    internal void OnStartUp(string id)
+    {
+        var mutexName = $"Global\\{id}";
+        
+        var mutexSecurity = new MutexSecurity();
+        mutexSecurity.AddAccessRule(new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null),
+            MutexRights.FullControl, AccessControlType.Allow));
+
+        _mutex = MutexAcl.Create(initiallyOwned: false, mutexName, out var newInstanceCreated, mutexSecurity);
+
+        if (newInstanceCreated) return;
+
+        MessageBox.Show("Another instance of the application is already running.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        _mutex.Close();
+        Environment.Exit(0);
+    }
+
+    internal void OnExit()
+    {
+        _mutex?.ReleaseMutex();
+        _mutex?.Close();
+    }
+}
